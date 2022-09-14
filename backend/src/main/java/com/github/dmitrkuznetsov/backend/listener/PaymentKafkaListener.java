31bbee1510ee57e54f17812f20c900a4ba5519b3
dmitrKuznetsov <kuznetsov1995dima@mail.ru>
2022-09-14 16:56:24 +0300

    Bug fixing

--- backend/src/main/java/com/github/dmitrkuznetsov/backend/listener/PaymentKafkaListener.java
+++ backend/src/main/java/com/github/dmitrkuznetsov/backend/listener/PaymentKafkaListener.java
@@ -4,7 +4,6 @@ import com.github.dmitrkuznetsov.backend.entity.Payment;
 import com.github.dmitrkuznetsov.backend.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.listener.MessageListener;
 import org.springframework.stereotype.Component;
 
@@ -14,9 +13,7 @@ public class PaymentKafkaListener implements MessageListener<Long, Payment> {
 
     private final PaymentService paymentService;
 
-    public PaymentKafkaListener(
-            @Qualifier("PaymentServiceImpl") PaymentService paymentService
-    ) {
+    public PaymentKafkaListener(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
